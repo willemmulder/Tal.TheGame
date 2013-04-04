cadc552889a2869a9218d506b13a97835abc43d0
willemmulder <willemmulder@gmail.com>
2013-04-04 19:29:54 +0200

    Added more game logic. Added proper rendering.

--- js/tal.js
+++ js/tal.js
@@ -1,4 +1,4 @@
-function Tal() {
+function TalGame() {
 
 	// Players
 	var playerCount = 2;
@@ -13,7 +13,10 @@ function Tal() {
 	// Game stats
 	var gameIsRunning = false;
 	var turnCount = 0;
-	var moves;
+	var moves = [];
+
+	// Callbacks
+	var callbacks = {};
 
 	var publics = {
 
@@ -48,26 +51,120 @@ function Tal() {
 
 		start : function() {
 			// Determine who is the start player
-			var startingPlayerIndex = Math.ceil(Math.random() * playerCount);
+			var startingPlayerIndex = Math.ceil(Math.random() * playerCount); // !Note: starts with 1
 			currentPlayerIndex = startingPlayerIndex;
 			currentPlayer = players[currentPlayerIndex];
 			// Reset game
 			gameIsRunning = true;
 			turnCount = 0;
+			moves = [];
 			privates.createBoard();
 			// Do first move
-			privates.playerMove();
+			privates.playerCanMove();
+		},
+
+		on : function(callbackName, callback) {
+			callbacks[callbackName] = callbacks[callbackName] || [];
+			callbacks[callbackName].push(callback);
+		},
+
+		off : function(callbackName, callback) {
+			// Remove specific callback
+			if (callbackName && callback) {
+				if (callbacks[callbackName]) {
+					var index = callbacks[callbackName].indexOf(callback);
+					if (index > -1) {
+						delete callbacks[callbackName][index];
+					}
+				}
+			// Remove all callbacks under this callbackName
+			} else if (callbackName) {
+				if (callbacks[callbackName]) {
+					delete callbacks[callbackName];
+				}
+			}
+		},
+
+		moveAllowed : function(from,to,playerIndex) {
+			// fromTile should exist
+			if (!board[from.y] || !board[from.y][from.x]) {
+				return false;
+			}
+			var fromTile = board[from.y][from.x];
+			// toTile should exist
+			if (!board[to.y] || !board[to.y][to.x]) {
+				return false;
+			}
+			var toTile = board[to.y][to.x];
+			// There should be a piece on the From tile
+			if (!fromTile.piece) {
+				return false;
+			}
+			var piece = fromTile.piece;
+			// The piece should be owned by the playerIndex' player
+			if (piece.playerIndex !== playerIndex) {
+				return false;
+			}
+			// TODO: validity checks on the move itself
+			return true;
 		}
 	}
 
 	var privates = {
 		createBoard : function() {
 			for(var i = 0; i < boardSize.height; i++) {
-				board
+				board[i] = [];
+				for (var i2 = 0; i2 < boardSize.width; i2++) {
+					board[i][i2] = {};
+				}
 			}
+			// Place pieces
+			board[0][5].piece = { type: "k", playerIndex : 1};
+			board[9][5].piece = { type: "k", playerIndex : 2};
 		},
-		playerMove : function() {
-			currentPlayer.doMove(board, (moves.length ? moves[moves.length] : false));
+		playerCanMove : function() {
+			currentPlayer.doMove(board, currentPlayerIndex, (moves.length ? moves[moves.length] : false)).done(function(move) {
+				// Process move
+				var from = move.from;
+				var to = move.to;
+				// Ensure that move is valid
+				if (!publics.moveAllowed(from,to,currentPlayerIndex)) {
+					// Let the player try another move
+					privates.trigger("invalidPlayerMove", move);
+					privates.playerCanMove();
+					return;
+				}
+				privates.movePiece(from,to);
+				// Indicate that player has moved
+				privates.trigger("validPlayerMove", move);
+				// Let other player move
+				currentPlayerIndex = privates.getNextPlayerIndex();
+				currentPlayer = players[currentPlayerIndex];
+				privates.playerCanMove();
+			}).fail(function() {
+				privates.trigger("error", "Player move did not execute");
+			});
+		},
+		movePiece : function(from,to) {
+			var fromTile = board[from.y][from.x];
+			var toTile = board[to.y][to.x];
+			toTile.piece = fromTile.piece;
+			delete fromTile.piece;			
+		},
+		getNextPlayerIndex : function() {
+			var next = currentPlayerIndex + 1;
+			if (next > playerCount) {
+				next = 1;
+			}
+			return next;
+		},
+		trigger : function(callbackName, args) {
+			if (callbacks[callbackName]) {
+				for(var index in callbacks[callbackName]) {
+					var callback = callbacks[callbackName][index];
+					callback.apply(this, args);
+				}
+			}
 		}
 	}
 
--- js/talCssRenderer.js
+++ js/talCssRenderer.js
@@ -1,3 +1,105 @@
 function TalCssRenderer(elm) {
-	
+
+	// Callbacks
+	var callbacks = {};
+	var canvas = $(elm);
+
+	// Interaction variables
+	var selectedTile;
+	var lastBoard;
+
+	// Make move if two tiles are clicked
+	canvas.on("click", ".tile", function() {
+		if (!selectedTile) {
+			selectedTile = this;
+			publics.reRender();
+			return;
+		} else {
+			var fromTile = $(selectedTile);
+			selectedTile = null;
+			var toTile = $(this);
+			publics.reRender();
+			privates.trigger("playerInput", {
+				from : {
+					x : fromTile.attr("column"), 
+					y : fromTile.attr("row")
+				},
+				to : {
+					x : toTile.attr("column"), 
+					y : toTile.attr("row")
+				}
+			});
+		}
+	});
+
+	var publics = {
+		render : function(board) {
+			lastBoard = board;
+			for (var rowIndex in board) {
+				var row = board[rowIndex];
+				for (var columnIndex in row) {
+					var tile = row[columnIndex];
+					var $tile = canvas.find(".row_"+rowIndex+".column_"+columnIndex);
+					if (!$tile.length) {
+						$tile = $("<div>").addClass("tile").addClass("row_"+rowIndex).addClass("column_"+columnIndex);
+						$tile.attr("column", columnIndex);
+						$tile.attr("row", rowIndex);
+						$tile.css("left", columnIndex*50 + "px");
+						$tile.css("top", rowIndex*50 + "px");
+						canvas.append($tile);
+					}
+					var piece = tile.piece;
+					if (piece) {
+						$tile.attr("playerIndex", piece.playerIndex);
+					} else {
+						$tile.removeAttr("playerIndex");
+					}
+					if (selectedTile === $tile.get(0)) {
+						$tile.addClass("selected");
+					} else {
+						$tile.removeClass("selected");
+					}
+				}
+			}
+		},
+
+		reRender : function() {
+			return publics.render(lastBoard);
+		},
+
+		on : function(callbackName, callback) {
+			callbacks[callbackName] = callbacks[callbackName] || [];
+			callbacks[callbackName].push(callback);
+		},
+
+		off : function(callbackName, callback) {
+			// Remove specific callback
+			if (callbackName && callback) {
+				if (callbacks[callbackName]) {
+					var index = callbacks[callbackName].indexOf(callback);
+					if (index > -1) {
+						delete callbacks[callbackName][index];
+					}
+				}
+			// Remove all callbacks under this callbackName
+			} else if (callbackName) {
+				if (callbacks[callbackName]) {
+					delete callbacks[callbackName];
+				}
+			}
+		}
+	}
+
+	var privates = {
+		trigger : function(callbackName) {
+			if (callbacks[callbackName]) {
+				for(var index in callbacks[callbackName]) {
+					var callback = callbacks[callbackName][index];
+					callback.apply(this, Array.prototype.slice.call(arguments,-1));
+				}
+			}
+		}
+	}
+
+	return publics;
 }
