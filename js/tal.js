3e11ec9ed804a5d93c26ce3a8b3fae9b9d0dcdb5
willemmulder <willemmulder@gmail.com>
2013-04-04 10:28:28 +0200

    Starting to implement core game engine

--- js/tal.js
+++ js/tal.js
@@ -1,3 +1,75 @@
 function Tal() {
-	
+
+	// Players
+	var playerCount = 2;
+	var players = [];
+	var currentPlayerIndex;
+	var currentPlayer;
+
+	// Board
+	var boardSize = {width:0,height:0};
+	var board = [];
+
+	// Game stats
+	var gameIsRunning = false;
+	var turnCount = 0;
+
+	var publics = {
+
+		playerCount : function(count) {
+			if (count) {
+				playerCount = count;
+			} else {
+				return count;
+			}
+		},
+
+		player : function(index, player) {
+			// Set player
+			if (index && player) {
+				players[index] = player;
+			// Return player
+			} else if (index) {
+				return players[index];
+			// If no parameter was passed, return false
+			} else {
+				return false;
+			}
+		},
+
+		boardSize : function(size) {
+			boardSize = size;
+		},
+
+		board : function() {
+			return board;
+		},
+
+		start : function() {
+			// Determine who is the start player
+			var startingPlayerIndex = Math.ceil(Math.random() * playerCount);
+			currentPlayerIndex = startingPlayerIndex;
+			currentPlayer = players[currentPlayerIndex];
+			// Reset game
+			gameIsRunning = true;
+			turnCount = 0;
+			privates.createBoard();
+			// Do first move
+			privates.playerMove();
+		}
+	}
+
+	var privates = {
+		createBoard : function() {
+			for(var i = 0; i < boardSize.height; i++) {
+				board
+			}
+		},
+		playerMove : function() {
+			currentPlayer.doMove();
+		}
+	}
+
+	return publics;
+
 }
