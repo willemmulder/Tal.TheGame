9b7378da18ec9f4f780391526f5a4f6aa2a3d241
willemmulder <willemmulder@gmail.com>
2013-04-11 08:22:47 +0200

    That bot is pretty tough to beat.

--- bots/willemSimpleBot.js
+++ bots/willemSimpleBot.js
@@ -18,12 +18,12 @@ TalGame.bots.willemSimpleBot = function(talGame) {
 
 			// Get some stats and make a move!
 			var pieces = talGame.piecesForPlayer(playerIndex);
+			var otherPlayerPieces = talGame.piecesForPlayer(playerIndex === 1 ? 2 : 1);
 			
 			// ===
 			// Other players force
 			// ===
 			// Find out the total force of the other player
-			var otherPlayerPieces = talGame.piecesForPlayer(playerIndex === 1 ? 2 : 1);
 			var currentTotalForce = 0;
 			$.each(otherPlayerPieces, function(index, piece) {
 				if (!piece.taken) {
@@ -34,12 +34,32 @@ TalGame.bots.willemSimpleBot = function(talGame) {
 			// ===
 			// Are we under attack?
 			// ===
-			var otherPlayerPieces = talGame.piecesForPlayer(playerIndex === 1 ? 2 : 1);
+			var currentTotalAttackedBy = 0;
+			$.each(pieces, function(index, piece) {
+				var attackingPieces = talGame.attackedByPiecesForPiece(piece, otherPlayerPieces);
+				if (attackingPieces.length) {
+					currentTotalAttackedBy += piece.number; // A higher number under attack is worse
+					if (piece.type === "k") {
+						// Never ever give away our king
+						currentTotalAttackedBy += (pieces.length * board.length);
+					}
+				}
+			});
+
+			// ===
+			// Do we attack?
+			// ===
 			var currentTotalAttack = 0;
 			$.each(pieces, function(index, piece) {
-				var attackingPieces = talGame.attackingPiecesForPiece(piece, otherPlayerPieces);
+				var attackingPieces = talGame.attackablePiecesForPiece(piece, otherPlayerPieces);
 				if (attackingPieces.length) {
-					currentTotalAttack += piece.number; // A higher number under attack is worse
+					$.each(attackingPieces, function(index2, otherPlayerPiece) {
+						currentTotalAttack += otherPlayerPiece.number; // A higher number under attack is better
+						if (otherPlayerPiece.type === "k") {
+							// Attacking their king is GREAT (most often)
+							currentTotalAttack += (otherPlayerPieces.length * board.length / 2);
+						}
+					});
 				}
 			});
 
@@ -59,7 +79,8 @@ TalGame.bots.willemSimpleBot = function(talGame) {
 			var highestMove;
 			var highestMoveDekking = 0;
 			var lowestTotalForce = 9999999999;
-			var lowestTotalAttack = 9999999999;
+			var lowestTotalAttackedBy = 9999999999;
+			var highestTotalAttack = 0;
 			var highestTotalImprovement = -100000; // A combination of dekking, force etc, compared to the current situation. Higher is better for us
 
 			var currentSituationBoard = talGame.board();
@@ -67,12 +88,14 @@ TalGame.bots.willemSimpleBot = function(talGame) {
 				var allowedMoves = talGame.allowedMovesForPiece(piece);
 				$.each(allowedMoves, function(index2, allowedMove) {
 					var newSituation = talGame.situationAfterMove(allowedMove, currentSituationBoard);
+					var otherPlayerPieces = newSituation.pieces[playerIndex === 1 ? 2 : 1];
+					var currentPlayerPieces = newSituation.pieces[playerIndex];
+
 					// ==
 					// Force
 					// ==
 					var newTotalForce = 0;
 					var kingPresent = false;
-					var otherPlayerPieces = newSituation.pieces[playerIndex === 1 ? 2 : 1];
 					$.each(otherPlayerPieces, function(index, piece) {
 						if (!piece.taken) {
 							newTotalForce += piece.number;
@@ -85,18 +108,43 @@ TalGame.bots.willemSimpleBot = function(talGame) {
 						lowestTotalForce = newTotalForce;
 					}
 
-					// ==
-					// Attack
-					// ==
-					var newTotalAttack = 0;
-					$.each(newSituation.pieces[playerIndex], function(index, newSituationPiece) {
-						var attackingPieces = talGame.attackingPiecesForPiece(newSituationPiece, otherPlayerPieces, newSituation.board);
-						if (attackingPieces.length) {
-							newTotalAttack += newSituationPiece.number; // A higher number under attack is worse
+					// ===
+					// Are we under attack?
+					// ===
+					var newTotalAttackedBy = 0;
+					var attackedByPieces;
+					$.each(currentPlayerPieces, function(index, piece) {
+						attackedByPieces = talGame.attackedByPiecesForPiece(piece, otherPlayerPieces, newSituation.board);
+						if (attackedByPieces.length) {
+							newTotalAttackedBy += piece.number; // A higher number under attack is worse
+							if (piece.type === "k") {
+								// Never ever give away our king
+								newTotalAttackedBy += (currentPlayerPieces.length * board.length);
+							}
 						}
 					});
-					if (newTotalAttack < lowestTotalAttack) {
-						lowestTotalAttack = newTotalAttack;
+					if (newTotalAttackedBy < lowestTotalAttackedBy) {
+						lowestTotalAttackedBy = newTotalAttackedBy;
+					}
+
+					// ===
+					// Do we attack?
+					// ===
+					var newTotalAttack = 0;
+					$.each(currentPlayerPieces, function(index, currentPlayerPiece) {
+						var attackingPieces = talGame.attackablePiecesForPiece(currentPlayerPiece, otherPlayerPieces, newSituation.board);
+						if (attackingPieces.length) {
+							$.each(attackingPieces, function(index2, otherPlayerPiece) {
+								newTotalAttack += otherPlayerPiece.number; // Attacking a higher number is better
+								if (otherPlayerPiece.type === "k") {
+									// Attacking their king is GREAT (most often)
+									newTotalAttack += (otherPlayerPieces.length * board.length / 2);
+								}
+							});
+						}
+					});
+					if (newTotalAttack > highestTotalAttack) {
+						highestTotalAttack = newTotalAttack;
 					}
 
 					// ==
@@ -115,12 +163,20 @@ TalGame.bots.willemSimpleBot = function(talGame) {
 					// Total
 					// ==
 					// If total is higher than we encountered thus far, save it
-					// We find totalForce 4 times more important than dekking and currentDekking 2 times more important than dekking
+					// We find totalForce 4 times more important than dekking 
+					// currentTotalAttackedBy 2 times more important than dekking
+					// currentTotalAttack also 2 times more important than dekking
+					/*if (currentTotalForce === 0) { currentTotalForce = 0.11; };
+					if (currentTotalAttack === 0) { currentTotalAttack = 0.11; };
+					if (currentTotalAttackedBy === 0) { currentTotalAttackedBy = 0.11; };
 					var totalImprovement = 
-						(currentTotalForce === 0 ? 0 : (currentTotalForce - newTotalForce)/currentTotalForce*4) + // percentage decrease in other player's force * 4
-						(currentTotalAttack === 0 ? 0 : (currentTotalAttack - newTotalAttack)/currentTotalAttack*2) +
+						((currentTotalForce - newTotalForce)/currentTotalForce*4) + // percentage decrease in other player's force * 4
+						((newTotalAttack - currentTotalAttack)/currentTotalAttack*2) + // increase
+						((currentTotalAttackedBy - newTotalAttackedBy)/currentTotalAttackedBy*2) + // decrease
 						// percentage decrease in attack by other player * 2
 						(newTotalDekking - currentTotalDekking)/currentTotalDekking // percentage increase in dekking
+					*/
+					var totalImprovement = - newTotalForce*4 - newTotalAttackedBy*4 + newTotalAttack*2 + newTotalDekking;
 					// If their king is gone, totalFactor is Infinity
 					if (!kingPresent) {
 						totalImprovement = Infinity;
@@ -128,18 +184,22 @@ TalGame.bots.willemSimpleBot = function(talGame) {
 					if (totalImprovement > highestTotalImprovement) {
 						highestTotalImprovement = totalImprovement;
 						highestMove = allowedMove;
+						//console.log("this is even better!", newTotalForce, newTotalAttackedBy, newTotalAttack, newTotalDekking, newSituation.board, attackedByPieces);
 					}
 				});
 			});
 			// Check highest dekking
+			/*
 			console.log("CURRENT", currentTotalDekking);
 			console.log("HIGHEST", highestMoveDekking);
 			console.log("current force", currentTotalForce);
 			console.log("lowest force", lowestTotalForce);
-			console.log("current attackedBy", currentTotalAttack);
-			console.log("lowest attackedBy", lowestTotalAttack);
+			console.log("current attackedBy", currentTotalAttackedBy);
+			console.log("lowest attackedBy", lowestTotalAttackedBy);
+			console.log("current attack", currentTotalAttack);
+			console.log("highest attack", highestTotalAttack);
 			console.log("best move", highestMove);
-			console.log("best totalFactor", highestTotalImprovement);
+			console.log("best totalFactor", highestTotalImprovement);*/
 			deferred.resolve(highestMove);
 			return deferred.promise();
 		}
